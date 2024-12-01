d7115fad43362102f6646d77aaf32e2200546781
unbe <unbe@users.noreply.github.com>
2024-12-01 19:07:44 +0100

    day1

--- day01.py
+++ day01.py
@@ -6,5 +6,5 @@ with open(sys.argv[1], 'r') as f:
 inp = [[int(x) for x in line.strip().split('   ')] for line in lines]
 srt = [sorted(x) for x in zip(*inp)]
 print('part1:', sum([abs(a-b) for a,b in zip(*srt)]))
-cnt=Counter(srt[1])
+cnt = Counter(srt[1])
 print('part2:', sum([cnt.get(x,0)*x for x in srt[0]]))
