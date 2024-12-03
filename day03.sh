c242d5af05395af4e947e51b2f7af78996f50098
unbe <unbe@users.noreply.github.com>
2024-12-03 22:17:49 +0100

    day3

--- day03.sh
+++ day03.sh
@@ -1,2 +1,5 @@
-egrep -o 'mul\(\d+,\d+\)' $1 | sed 's/mul.//g; s/,/*/; s/)/+/; $ s/+//'  | tr '\n' ' ' | bc -l
-cat $1 | tr '\n' ' ' | sed "s/do()/\n/g"  | sed "s/don't().*//g" | egrep -o 'mul\(\d+,\d+\)' | sed 's/mul.//g; s/,/*/; s/)/+/; $ s/+//'  | tr '\n' ' ' | bc -l
+part1 () { egrep -o 'mul\(\d+,\d+\)' | sed 's/mul.//g; s/,/*/; s/)/+/; $ s/+//'  | tr '\n' ' ' | bc -l; }
+echo 'part1: '
+cat $1 | part1
+echo 'part2: '
+cat $1 | tr '\n' ' ' | sed "s/do()/\n/g"  | sed "s/don't().*//g" | part1
