4856add74650ef81391c1c444e6cb2486fd90e52
unbe <unbe@users.noreply.github.com>
2024-12-03 22:36:32 +0100

    day3 without cats

--- day03.sh
+++ day03.sh
@@ -1,5 +1,5 @@
 part1 () { egrep -o 'mul\(\d+,\d+\)' | sed 's/mul.//g; s/,/*/; s/)/+/; $ s/+//'  | tr '\n' ' ' | bc -l; }
 echo 'part1: '
-cat $1 | part1
+part1 < $1
 echo 'part2: '
-cat $1 | tr '\n' ' ' | sed "s/do()/\n/g"  | sed "s/don't().*//g" | part1
+tr '\n' ' ' < $1 | sed "s/do()/\n/g"  | sed "s/don't().*//g" | part1
