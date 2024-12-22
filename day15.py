180f9e3287d718cb252255561ff9c42b67db976e
unbe <unbe@users.noreply.github.com>
2024-12-22 20:58:08 +0100

    day15 with many less subtle bugs on the way

--- day15.py
+++ day15.py
@@ -1,53 +1,64 @@
 import sys
 import re
+import operator
 from functools import reduce
-from operator import mul
+
+def solve(mp, moves):
+  dirs={}
+  dirs['<'] = (0, -1)
+  dirs['>'] = (0, 1)
+  dirs['^'] = (-1, 0)
+  dirs['v'] = (1, 0)
+
+  pos, = [cr for cr in map(lambda ic: (ic[0], ic[1].index('@') if '@' in ic[1] else -1), enumerate(mp)) if cr[1] >= 0]
+  mp[pos[0]][pos[1]] = '.'
+
+  for mv in moves:
+    dr = dirs[mv]
+    front = [pos]
+    shape = [front]
+    blocked = False
+    npos = None
+    while len(front) > 0 and not blocked:
+      nfront = set()
+      for f in front:
+        nf = (f[0] + dr[0], f[1] + dr[1])
+        if mp[nf[0]][nf[1]] == '#':
+          blocked = True
+          break
+        if npos is None:
+          npos = nf
+        if mp[nf[0]][nf[1]] == '.':
+          continue
+        nfront.add(nf)
+        if dr[0] != 0 and mp[nf[0]][nf[1]] == '[':
+          nfront.add((nf[0], nf[1]+1))
+        if dr[0] != 0 and mp[nf[0]][nf[1]] == ']':
+          nfront.add((nf[0], nf[1]-1))
+      front = nfront
+      shape.insert(0, front)
+    if blocked:
+      continue
+    for front in shape:
+      for s in front:
+        mp[s[0] + dr[0]][s[1] + dr[1]] = mp[s[0]][s[1]]
+        mp[s[0]][s[1]] = '.'
+    pos = npos
+  return sum([100*i + j if c in '[O' else 0 for i in range(len(mp)) for j, c in enumerate(mp[i])])
+
 
 with open(sys.argv[1], 'r') as f:
   lines = list(map(lambda l: list(l.strip()), f.readlines()))
-  eline = lines.index([])
-  mp = lines[:eline]
-  moves = ''.join(map(lambda l: ''.join(l), lines[eline+1:]))
+eline = lines.index([])
+map1 = lines[:eline]
+moves = ''.join(map(lambda l: ''.join(l), lines[eline+1:]))
 
-print('\n'.join(map(lambda l: ''.join(l), mp)), moves)
+repls = {}
+repls['#'] = '##'
+repls['O'] = '[]'
+repls['.'] = '..'
+repls['@'] = '@.'
+map2 = list(map(lambda line: reduce(operator.add, [list(repls[x]) for x in line]), map1))
 
-r, = [r for r in map(lambda ic: (ic[0], ic[1].index('@') if '@' in ic[1] else -1), enumerate(mp)) if r[1] >= 0]
-print(r)
-mp[r[0]][r[1]] = '.'
-print('\n'.join(map(lambda l: ''.join(l), mp)), r)
-
-dirs={}
-dirs['<'] = (0, -1)
-dirs['>'] = (0, 1)
-dirs['^'] = (-1, 0)
-dirs['v'] = (1, 0)
-
-for mv in moves:
-  dr = dirs[mv]
-  newpos = None
-  box = r
-  while True:
-    box = (box[0] + dr[0], box[1] + dr[1])
-    if mp[box[0]][box[1]] != 'O':
-      break
-    if newpos is None:
-      newpos = box
-  if mp[box[0]][box[1]] == '#':
-    continue
-  if mp[box[0]][box[1]] != '.':
-    raise 'What??'
-  if newpos is not None:
-    mp[box[0]][box[1]] = 'O'
-    mp[newpos[0]][newpos[1]] = '.'
-    r = newpos
-  else:
-    r = box
-print('\n'.join(map(lambda l: ''.join(l), mp)), r)
-sum = 0
-for i in range(len(mp)):
-  for j in range(len(mp[i])):
-    if mp[i][j] == 'O':
-      sum += 100*i + j
-print(sum)
-
-
+print('part1:', solve(map1, moves))
+print('part2:', solve(map2, moves))
